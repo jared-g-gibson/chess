fed07f4affd422afb6938a66555cd793cee2278a
Jared <jared.g.gibson32@gmail.com>
2024-02-24 00:37:56 -0700

    Cleaned up some code in KingMovesCalculator and KnightMovesCalculator

--- shared/src/main/java/chess/pieces/KingMovesCalculator.java
+++ shared/src/main/java/chess/pieces/KingMovesCalculator.java
@@ -17,54 +17,50 @@ public class KingMovesCalculator implements PieceMovesCalculator {
         int row = myPosition.getRow();
         int col = myPosition.getColumn();
 
+        // Create instance of Knight Moves Calculator to get
+        // access to isValidMove method
+        KnightMovesCalculator calculator = new KnightMovesCalculator(pieceColor);
+
         // Check if King can move up
-        ChessMove move = kingHelper(board, myPosition, row + 1, col, pieceColor);
+        ChessMove move = calculator.isValidMove(board, myPosition, row + 1, col, pieceColor);
         if(move != null)
             moves.add(move);
 
         // Check if King can move down
-        move = kingHelper(board, myPosition, row - 1, col, pieceColor);
+        move = calculator.isValidMove(board, myPosition, row - 1, col, pieceColor);
         if(move != null)
             moves.add(move);
 
         // Check if King can move left
-        move = kingHelper(board, myPosition, row, col - 1, pieceColor);
+        move = calculator.isValidMove(board, myPosition, row, col - 1, pieceColor);
         if(move != null)
             moves.add(move);
 
         // Check if King can move right
-        move = kingHelper(board, myPosition, row, col + 1, pieceColor);
+        move = calculator.isValidMove(board, myPosition, row, col + 1, pieceColor);
         if(move != null)
             moves.add(move);
 
         // Check if King can move up and to the right
-        move = kingHelper(board, myPosition, row + 1, col + 1, pieceColor);
+        move = calculator.isValidMove(board, myPosition, row + 1, col + 1, pieceColor);
         if(move != null)
             moves.add(move);
 
         // Check if King can move up and to the left
-        move = kingHelper(board, myPosition, row + 1, col - 1, pieceColor);
+        move = calculator.isValidMove(board, myPosition, row + 1, col - 1, pieceColor);
         if(move != null)
             moves.add(move);
 
         // Check if King can move down and to the right
-        move = kingHelper(board, myPosition, row - 1, col + 1, pieceColor);
+        move = calculator.isValidMove(board, myPosition, row - 1, col + 1, pieceColor);
         if(move != null)
             moves.add(move);
 
         // Check if King can move down and to the left
-        move = kingHelper(board, myPosition, row - 1, col - 1, pieceColor);
+        move = calculator.isValidMove(board, myPosition, row - 1, col - 1, pieceColor);
         if(move != null)
             moves.add(move);
 
         return moves;
-    }
-    public static ChessMove kingHelper(ChessBoard board, ChessPosition myPosition, int row, int col, ChessGame.TeamColor pieceColor) {
-        if(row > 0 && row < 9 && col > 0 && col < 9 && board.getPiece(new ChessPosition(row, col)) == null)
-            return new ChessMove(myPosition, new ChessPosition(row, col), null);
-        else if(row > 0 && row < 9 && col > 0 && col < 9 && board.getPiece(new ChessPosition(row, col)).getTeamColor() != pieceColor)
-            return new ChessMove(myPosition, new ChessPosition(row, col), null);
-        else
-            return null;
     }
 }
--- shared/src/main/java/chess/pieces/KnightMovesCalculator.java
+++ shared/src/main/java/chess/pieces/KnightMovesCalculator.java
@@ -25,41 +25,41 @@ public class KnightMovesCalculator implements PieceMovesCalculator{
         int col = myPosition.getColumn();
 
         // Up and left combos
-        ChessMove move = knightHelper(board, myPosition, row + 1, col - 2, pieceColor);
+        ChessMove move = isValidMove(board, myPosition, row + 1, col - 2, pieceColor);
         if(move != null)
             moves.add(move);
-        move = knightHelper(board, myPosition, row + 2, col - 1, pieceColor);
+        move = isValidMove(board, myPosition, row + 2, col - 1, pieceColor);
         if(move != null)
             moves.add(move);
 
         // Up and right combos
-        move = knightHelper(board, myPosition, row + 1, col + 2, pieceColor);
+        move = isValidMove(board, myPosition, row + 1, col + 2, pieceColor);
         if(move != null)
             moves.add(move);
-        move = knightHelper(board, myPosition, row + 2, col + 1, pieceColor);
+        move = isValidMove(board, myPosition, row + 2, col + 1, pieceColor);
         if(move != null)
             moves.add(move);
 
         // Down and left combos
-        move = knightHelper(board, myPosition, row - 1, col - 2, pieceColor);
+        move = isValidMove(board, myPosition, row - 1, col - 2, pieceColor);
         if(move != null)
             moves.add(move);
-        move = knightHelper(board, myPosition, row - 2, col - 1, pieceColor);
+        move = isValidMove(board, myPosition, row - 2, col - 1, pieceColor);
         if(move != null)
             moves.add(move);
 
         // Down and right combos
-        move = knightHelper(board, myPosition, row - 1, col + 2, pieceColor);
+        move = isValidMove(board, myPosition, row - 1, col + 2, pieceColor);
         if(move != null)
             moves.add(move);
-        move = knightHelper(board, myPosition, row - 2, col + 1, pieceColor);
+        move = isValidMove(board, myPosition, row - 2, col + 1, pieceColor);
         if(move != null)
             moves.add(move);
 
         return moves;
     }
 
-    public static ChessMove knightHelper(ChessBoard board, ChessPosition myPosition, int row, int col, ChessGame.TeamColor pieceColor) {
+    public ChessMove isValidMove(ChessBoard board, ChessPosition myPosition, int row, int col, ChessGame.TeamColor pieceColor) {
         if(row > 0 && row < 9 && col > 0 && col < 9 && board.getPiece(new ChessPosition(row, col)) == null)
             return new ChessMove(myPosition, new ChessPosition(row, col), null);
         else if(row > 0 && row < 9 && col > 0 && col < 9 && board.getPiece(new ChessPosition(row, col)).getTeamColor() != pieceColor)
