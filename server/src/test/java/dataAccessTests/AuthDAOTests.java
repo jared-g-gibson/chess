5b10b13ec4a0570ebfa6f6c11bab572468c79fbc
Jared <jared.g.gibson32@gmail.com>
2024-03-06 12:07:22 -0700

    Created Tests for AuthDAO

--- server/src/test/java/dataAccessTests/AuthDAOTests.java
+++ server/src/test/java/dataAccessTests/AuthDAOTests.java
@@ -3,7 +3,6 @@ package dataAccessTests;
 import dataAccess.AuthDAO;
 import dataAccess.MemoryAuthDAO;
 import dataAccess.SQLAuthDAO;
-import model.UserData;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -30,6 +29,136 @@ public class AuthDAOTests {
         }
         catch (Exception e) {
             throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    public void createAuthPass() {
+        AuthDAO auths = new SQLAuthDAO();
+        AuthDAO memoryAuths = new MemoryAuthDAO();
+        try {
+            // SQL
+            String authToken = auths.createAuth("Joe");
+            Assertions.assertNotNull(auths.getAuth(authToken));
+
+            // Memory
+            authToken = memoryAuths.createAuth("Joe");
+            Assertions.assertNotNull(memoryAuths.getAuth(authToken));
+
+            // Clear for next test
+            auths.clear();
+            memoryAuths.clear();
+        }
+        catch (Exception e) {
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+
+    // TODO:
+    /*@Test
+    public void createAuthFail() {
+        try {
+
+        }
+        catch (Exception e) {
+            throw new RuntimeException(e.getMessage());
+        }
+    }*/
+
+    @Test
+    public void getAuthPass() {
+        AuthDAO auths = new SQLAuthDAO();
+        AuthDAO memoryAuths = new MemoryAuthDAO();
+        try {
+            // SQL
+            String authToken = auths.createAuth("Jake");
+            Assertions.assertNotNull(auths.getAuth(authToken));
+
+            // Memory
+            authToken = memoryAuths.createAuth("Jake");
+            Assertions.assertNotNull(memoryAuths.getAuth(authToken));
+
+            // Clear for next test
+            auths.clear();
+            memoryAuths.clear();
+        }
+        catch (Exception e) {
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+
+    @Test
+    public void getAuthFail() {
+        AuthDAO auths = new SQLAuthDAO();
+        AuthDAO memoryAuths = new MemoryAuthDAO();
+        try {
+            // SQL
+            String authToken = auths.createAuth("Joe");
+            Assertions.assertNull(auths.getAuth("Wrong authToken"));
+
+            // Memory
+            authToken = memoryAuths.createAuth("Jake");
+            Assertions.assertNull(memoryAuths.getAuth("Wrong authToken"));
+
+            // Clear for next test
+            auths.clear();
+            memoryAuths.clear();
+        }
+        catch (Exception e) {
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+
+    @Test
+    public void deleteAuthPass() {
+        AuthDAO auths = new SQLAuthDAO();
+        AuthDAO memoryAuths = new MemoryAuthDAO();
+        try {
+            // SQL
+            String authToken = auths.createAuth("Joe");
+            Assertions.assertNotNull(auths.getAuth(authToken));
+            auths.deleteAuth(authToken);
+            Assertions.assertNull(auths.getAuth(authToken));
+
+            // Memory
+            authToken = memoryAuths.createAuth("Joe");
+            Assertions.assertNotNull(memoryAuths.getAuth(authToken));
+            memoryAuths.deleteAuth(authToken);
+            Assertions.assertNull(auths.getAuth(authToken));
+
+            // Clear for next test
+            auths.clear();
+            memoryAuths.clear();
+        }
+        catch (Exception e) {
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+
+    // TODO:
+    @Test
+    public void deleteAuthFail() {
+        AuthDAO auths = new SQLAuthDAO();
+        AuthDAO memoryAuths = new MemoryAuthDAO();
+        try {
+            // SQL
+            String authToken = auths.createAuth("Joe");
+            Assertions.assertNotNull(auths.getAuth(authToken));
+            auths.deleteAuth("Wrong authToken");
+            Assertions.assertNull(auths.getAuth(authToken));
+
+            // Memory
+            authToken = memoryAuths.createAuth("Joe");
+            Assertions.assertNotNull(memoryAuths.getAuth(authToken));
+            memoryAuths.deleteAuth("Wrong authToken");
+            Assertions.assertNull(auths.getAuth(authToken));
+
+            // Clear for next test
+            auths.clear();
+            memoryAuths.clear();
+        }
+        catch (Exception e) {
+            throw new RuntimeException(e.getMessage());
         }
     }
 }
