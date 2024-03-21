39cd19cfbedcb4fbdcdb499b72de3090107c6024
Jared <jared.g.gibson32@gmail.com>
2024-03-21 02:21:14 -0600

    Edited the GameplayUI Chess board

--- client/src/main/java/repl/GameplayUI.java
+++ client/src/main/java/repl/GameplayUI.java
@@ -35,7 +35,7 @@ public class GameplayUI {
     }
 
     private void printHeaders() {
-        System.out.print("   " + SET_TEXT_BOLD);
+        System.out.print("    " + SET_TEXT_BOLD);
         for (String header : headers) {
             System.out.print(header + "   ");
         }
@@ -43,7 +43,7 @@ public class GameplayUI {
     }
 
     private void printHeadersBackwards() {
-        System.out.print("   " + SET_TEXT_BOLD);
+        System.out.print("    " + SET_TEXT_BOLD);
         for (int x = 7; x >= 0; x--) {
             System.out.print(headers[x] + "   ");
         }
@@ -54,25 +54,25 @@ public class GameplayUI {
         switch (row) {
             case 8 -> {
                 System.out.print("8 " + SET_TEXT_BOLD);
-                System.out.println(BLACK_ROOK + "|" + BLACK_KNIGHT + "|" + BLACK_BISHOP + "|" + BLACK_KING + "|"  + BLACK_QUEEN + "|" + BLACK_BISHOP + "|" + BLACK_KNIGHT + "|" + BLACK_ROOK + "| 8");
+                System.out.println("|" + BLACK_ROOK + "|" + BLACK_KNIGHT + "|" + BLACK_BISHOP + "|" + BLACK_KING + "|"  + BLACK_QUEEN + "|" + BLACK_BISHOP + "|" + BLACK_KNIGHT + "|" + BLACK_ROOK + "| 8");
                 //System.out.println("R   N   B   K   Q   B   N   R    1");
             }
             case 7 -> {
                 System.out.print("7 " + SET_TEXT_BOLD);
-                System.out.println(BLACK_PAWN + "|" + BLACK_PAWN + "|" + BLACK_PAWN + "|" + BLACK_PAWN + "|"  + BLACK_PAWN + "|" + BLACK_PAWN + "|" + BLACK_PAWN + "|" + BLACK_PAWN + "| 2");
+                System.out.println("|" + BLACK_PAWN + "|" + BLACK_PAWN + "|" + BLACK_PAWN + "|" + BLACK_PAWN + "|"  + BLACK_PAWN + "|" + BLACK_PAWN + "|" + BLACK_PAWN + "|" + BLACK_PAWN + "| 2");
             }
             case 2 -> {
                 System.out.print("2 " + SET_TEXT_BOLD);
-                System.out.println(WHITE_PAWN + "|" + WHITE_PAWN + "|" + WHITE_PAWN + "|" + WHITE_PAWN + "|"  + WHITE_PAWN + "|" + WHITE_PAWN + "|" + WHITE_PAWN + "|" + WHITE_PAWN + "| 2");
+                System.out.println("|" + WHITE_PAWN + "|" + WHITE_PAWN + "|" + WHITE_PAWN + "|" + WHITE_PAWN + "|"  + WHITE_PAWN + "|" + WHITE_PAWN + "|" + WHITE_PAWN + "|" + WHITE_PAWN + "| 2");
                 //System.out.println("P   P   P   P   P   P   P   P    2");
             }
             case 1 -> {
                 System.out.print("1 " + SET_TEXT_BOLD);
-                System.out.println(WHITE_ROOK + "|" + WHITE_KNIGHT + "|" + WHITE_BISHOP + "|" + WHITE_KING + "|"  + WHITE_QUEEN + "|" + WHITE_BISHOP + "|" + WHITE_KNIGHT + "|" + WHITE_ROOK + "| 1");
+                System.out.println("|" + WHITE_ROOK + "|" + WHITE_KNIGHT + "|" + WHITE_BISHOP + "|" + WHITE_KING + "|"  + WHITE_QUEEN + "|" + WHITE_BISHOP + "|" + WHITE_KNIGHT + "|" + WHITE_ROOK + "| 1");
 
                 //System.out.println("R   N   B   K   Q   B   N   R    1");
             }
-            default -> System.out.println(SET_TEXT_BOLD + row + "    |   |   |   |   |   |   |   | " + row);
+            default -> System.out.println(SET_TEXT_BOLD + row + " |   |   |   |   |   |   |   |   | " + row);
         }
 
         /*if(row == 1) {
