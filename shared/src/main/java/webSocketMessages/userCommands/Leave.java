90f7a2e20d7cb632a494cfafb1f20f3c28c3c888
Jared <jared.g.gibson32@gmail.com>
2024-04-09 12:24:04 -0600

    Created UserGameCommand classes

---
+++ shared/src/main/java/webSocketMessages/userCommands/Leave.java
@@ -0,0 +1,10 @@
+package webSocketMessages.userCommands;
+
+public class Leave extends  UserGameCommand {
+
+    private int gameID;
+    public Leave(String authToken, int gameID) {
+        super(authToken);
+        this.gameID = gameID;
+    }
+}
