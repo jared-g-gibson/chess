80bad1fdf5663e56d44745bf6402c7a0a12ec150
Jared <jared.g.gibson32@gmail.com>
2024-04-09 12:24:53 -0600

    Created classes for websockets using Professor Rodham's diagram.

---
+++ server/src/main/java/server/WebSocketSessions.java
@@ -0,0 +1,40 @@
+package server;
+
+import javax.websocket.Session;
+import java.util.HashMap;
+import java.util.Map;
+
+public class WebSocketSessions {
+
+    // Implementation from Rodham Diagram
+    private Map<Integer, Map<String, Session>> sessionMap;
+
+    public WebSocketSessions() {
+        sessionMap = new HashMap<>();
+    }
+    public void addSessionToGame(int gameID, String authToken, Session session) {
+        Map<String, Session> map = new HashMap<>();
+        map.put(authToken, session);
+        sessionMap.put(gameID, map);
+    }
+
+    public void removeSessionFromGame(int gameID, String authToken, Session session) {
+        Map<String, Session> map = new HashMap<>();
+        map.put(authToken, session);
+        sessionMap.remove(gameID, map);
+    }
+
+    public void removeSession(Session session) {
+        for(Map.Entry<Integer, Map<String, Session>> set : sessionMap.entrySet()) {
+            for(Map.Entry<String, Session> mySet : set.getValue().entrySet()) {
+                if(mySet.getValue().equals(session))
+                    sessionMap.remove(set.getKey(), set.getValue());
+            }
+        }
+    }
+
+    public Map<String, Session> getSessionsForGame(int gameID) {
+        return sessionMap.get(gameID);
+    }
+
+}
